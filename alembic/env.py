811fee4c070d1fd0ac45ed45a519af093efb0e54
Enrico <enricodeabm@hotmail.com>
2026-08-09 18:54:29 -0300

    change: alterando url do banco de dados

--- alembic/env.py
+++ alembic/env.py
@@ -7,15 +7,25 @@ from alembic import context
 
 from app.infra.database import Base
 from app.models.user import User
+from app.infra.config import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+database_url = Settings.DATABASE_URL
+
+config.set_main_option(
+    "sqlalchemy.url",
+    database_url
+)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+
 
 # add your model's MetaData object here
 # for 'autogenerate' support
